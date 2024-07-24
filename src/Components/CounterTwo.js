c4972dbd5cecc7c1a096975a75c9accf677f0fca
Yanil01 <idoyanil45@gmail.com>
2024-07-24 22:40:57 +0545

    state as objects

--- src/Components/CounterTwo.js
+++ src/Components/CounterTwo.js
@@ -3,15 +3,20 @@ import React, { useReducer } from "react";
 // Initial state for the counter component
 const initialState = {
   firstCounter: 0, // Initial value of firstCounter
+  secondCounter: 10, // Initial value of secondCounter
 };
 
 // Reducer function that determines how state changes based on actions
 const reducer = (state, action) => {
   switch (action.type) {
     case "increment":
-      return { firstCounter: state.firstCounter + 1 }; // Increase firstCounter by 1
+      return { ...state, firstCounter: state.firstCounter + action.value }; // Increment firstCounter by action.value
     case "decrement":
-      return { firstCounter: state.firstCounter - 1 }; // Decrease firstCounter by 1
+      return { ...state, firstCounter: state.firstCounter - action.value }; // Decrement firstCounter by action.value
+    case "increment2":
+      return { ...state, secondCounter: state.secondCounter + action.value }; // Increment firstCounter by action.value
+    case "decrement2":
+      return { ...state, secondCounter: state.secondCounter - action.value }; // Decrement firstCounter by action.value
     case "reset":
       return initialState; // Reset state to initial state
     default:
@@ -26,30 +31,70 @@ function CounterTwo() {
   return (
     <div>
       {/* Displaying the current value of firstCounter */}
-      <div>count : {count.firstCounter}</div>
+      <div>First Counter : {count.firstCounter}</div>
+      {/* Displaying the current value of secondCounter */}
 
-      {/* Button to increment firstCounter */}
+      <div>Second Counter : {count.secondCounter}</div>
+
+      {/* Button to increment firstCounter by 1 */}
       <button
         onClick={() => {
-          dispatch({ type: "increment" }); // Dispatching action to increment firstCounter
+          dispatch({ type: "increment", value: 1 });
         }}
       >
         Increment
       </button>
 
-      {/* Button to decrement firstCounter */}
+      {/* Button to decrement firstCounter by 1 */}
       <button
         onClick={() => {
-          dispatch({ type: "decrement" }); // Dispatching action to decrement firstCounter
+          dispatch({ type: "decrement", value: 1 });
         }}
       >
         Decrement
       </button>
 
+      {/* Button to increment firstCounter by 5 */}
+      <button
+        onClick={() => {
+          dispatch({ type: "increment", value: 5 });
+        }}
+      >
+        Increment 5
+      </button>
+
+      {/* Button to decrement firstCounter by 5 */}
+      <button
+        onClick={() => {
+          dispatch({ type: "decrement", value: 5 });
+        }}
+      >
+        Decrement 5
+      </button>
+      <div>
+        {/* Button to increment secondCounter by 1 */}
+        <button
+          onClick={() => {
+            dispatch({ type: "increment2", value: 1 });
+          }}
+        >
+          Increment Counter 2
+        </button>
+
+        {/* Button to decrement secondCounter by 1 */}
+        <button
+          onClick={() => {
+            dispatch({ type: "decrement2", value: 1 });
+          }}
+        >
+          Decrement Counter 2
+        </button>
+      </div>
+
       {/* Button to reset state to initial state */}
       <button
         onClick={() => {
-          dispatch({ type: "reset" }); // Dispatching action to reset state
+          dispatch({ type: "reset" });
         }}
       >
         Reset
